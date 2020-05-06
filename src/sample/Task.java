6e8dbc5fa646778f41d67a9149c35ad588fa1eec
HelgaBK <olga.karpyshyn@gmail.com>
2020-05-06 22:56:05 +0300

    addded methods getScene, getShape, DrawPoint,

--- src/sample/Task.java
+++ src/sample/Task.java
@@ -4,9 +4,12 @@ import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Task {
     static final int HEIGHT = 700;
@@ -18,10 +21,10 @@ public class Task {
     static Group item_group;
 
     public Task() {
-        this.Setup;
+        this.Setup();
     }
 
-    public void Setup{
+    public void Setup(){
     item_group = new Group();
         List<Point2D> points = getShape();
         List<Point2D> shape = JarvisAlgorithm(points);
@@ -29,7 +32,13 @@ public class Task {
         for(Point2D point : shape){
             DrawPoint(point, Color.DARKRED, 5);
         }
+        DrawShape(shape);
+        for(Point2D point : points){
+            DrawPoint(point, Color.BLACK, 2);
+        }
+
         shape.add(new Point2D(shape.get(0).getX(), shape.get(0).getY()));
+
         //Casteljo(shape);
         List<Float> shapeFloat = new ArrayList<>();
         for (Point2D p : shape) {
@@ -37,21 +46,69 @@ public class Task {
             shapeFloat.add((float) p.getY());
         }
         Spline.drawSpline(shapeFloat, 0.33f, true);
-
-
-    }
-
-    private void DrawPoint(Point2D point, Color color, int radius) {
-    }
-
-    private List<Point2D> JarvisAlgorithm(List<Point2D> points) {
-    }
-
-
-    private List<Point2D> getShape() {
     }
 
 
     public Scene getScene() {
+        return new Scene(item_group, WIDTH, HEIGHT);
     }
+
+    private List<Point2D> getShape() {
+        Random random = new Random();
+        int count = (random.nextInt() % POINT_COUNT) + POINT_COUNT/2;
+        if(count < 100) count = 3000;
+        ArrayList<Point2D> res = new ArrayList<>();
+        while(res.size() < 15){
+            int x = random.nextInt(1100);
+            int y = random.nextInt(700);
+            boolean b = random.nextBoolean();
+            Point2D point;
+            if(Math.sqrt(Math.pow(650 - x, 2) + Math.pow(350 - y, 2)) < 320){
+                if(b)
+                    point = new Point2D(x+50, y);
+                else
+                    point = new Point2D(x - 50, y);
+
+                res.add(point);
+            }
+        }
+        return res;
+    }
+
+    private void DrawShape(List<Point2D> shape){
+        for (int i = 0; i < shape.size(); i++) {
+            int j = (i + 1) % shape.size();
+            Line l = new Line(shape.get(i).getX(), HEIGHT - shape.get(i).getY(), shape.get(j).getX(), HEIGHT - shape.get(j).getY());
+            l.setStroke(Color.BLUE);
+            item_group.getChildren().add(l);
+        }
+    }
+
+    private void DrawPoint(Point2D point, Color color, int radius) {
+        Circle circle = new Circle(radius);
+        circle.setFill(color);
+        circle.setTranslateX(point.getX());
+        circle.setTranslateY(HEIGHT - point.getY());
+        item_group.getChildren().add(circle);
+    }
+
+    private Point2D SmallestPoint(List<Point2D> shape){
+        Point2D p = shape.get(0);
+        for(Point2D point2D : shape)
+
+            if(point2D.getY() < p.getY())
+                p = point2D;
+            else if (point2D.getY() == p.getY() && point2D.getX() < p.getX())
+                p = point2D;
+            return p;
+    }
+
+    private List<Point2D> JarvisAlgorithm(List<Point2D> points) {
+        
+    }
+
+
+
+
+
 }
